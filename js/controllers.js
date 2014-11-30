6ebceb1e71f10257c80b255c1329e5b9e8a74270
Dev <dev@Devs-MacBook-Pro.local>
2014-11-30 18:50:22 +0700

    Add review section in product-detail page

--- js/controllers.js
+++ js/controllers.js
@@ -4,51 +4,15 @@
 
 var StoreController = angular.module('StoreController', []);
 
-// 1 - phonecat
-// seatpadStore.controller('StoreController', function($scope, $http) {
-//   $http.get('data/seatpads.json').success(function(data) {
-//     $scope.products = data;
-//   });
-
-//   $scope.orderProp = 'age';
-// });
- 
-// 1 - koko
-// StoreController.controller("ProductListCtrl", function($scope, $http) {
-// 	$http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
-// 	$http.get('data/seatpads.json').success(function(data, status, headers, config) {
-//     $scope.products = data;
-//   });
-// });
-
-
-// 1
-// StoreController.controller('ProductDetailCtrl', ['$scope', '$routeParams', 
-// 	function($scope, $routeParams) {
-//   	$scope.productId = $routeParams.productId;
-// }]);
-
-// 2
-// StoreController.controller('ProductDetailCtrl', ['$scope', '$routeParams', '$http',
-//   function($scope, $routeParams, $http) {
-//     $http.get('data/' + $routeParams.productId + '.json').success(function(data) {
-//       console.log($routeParams.productId + "-" + data);
-//       $scope.product = data;
-//       $scope.mainImageUrl = data.images[0];
-//     });
-//     $scope.setImage = function(imageUrl) {
-//       $scope.mainImageUrl = imageUrl;
-//     }
-//   }]);
-
-StoreController.controller('ProductListCtrl', ['$scope', 'Seatpad', 
+StoreController.controller('ProductListCtrl', ['$scope', 'Seatpad',
   function($scope, Seatpad) {
     $scope.products = Seatpad.query();
     $scope.orderSort = 'id';
+
   }
 ]);
 
-StoreController.controller('ProductDetailCtrl', ['$scope', '$routeParams', 'Seatpad', 
+StoreController.controller('ProductDetailCtrl', ['$scope', '$routeParams', 'Seatpad',
 	function($scope, $routeParams, Seatpad) {
   $scope.product = Seatpad.get({productId: $routeParams.productId}, function(product) {
     $scope.mainImageUrl = product.images[0];
@@ -58,4 +22,16 @@ StoreController.controller('ProductDetailCtrl', ['$scope', '$routeParams', 'Seat
   $scope.setImage = function(imageUrl) {
     $scope.mainImageUrl = imageUrl;
   }
-}]);
+
+    $scope.review = {};
+    $scope.addReview = function(product){
+      product.reviews.push($scope.review);
+      $scope.review = {};
+    };
+  }]);
+
+// StoreController.controller('ReviewController', ['$scope', 'Seatpad',
+//   function($scope, Seatpad){
+
+//   }
+// ]);
